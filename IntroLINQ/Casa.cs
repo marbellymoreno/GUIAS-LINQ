a3653bcfbe38a39d0cf977324ddc542c17f580f7
Marbelly <me0763032023@unab.edu.sv>
2024-08-30 15:20:39 -0600

    Agregar archivos de proyecto.

---
+++ IntroLINQ/Casa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroLINQ
+{
+    public class Casa
+    {      
+        public int Id { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public int numeroHabitaciones { get; set; }
+
+        public string datosCasa()
+        {
+            return $"La direccion es {Direccion}, en la ciudad{Ciudad}";
+        }
+    }
+}
